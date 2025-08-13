1fab4e6f3faef7b60c5e62f04669a0f7641eb0b5
lilkasalananshortie <manalilimhakujose@gmail.com>
2025-08-13 20:55:11 +0800

    <Final Update All done>

--- CalculatorApplication/CalculatorClass.cs
+++ CalculatorApplication/CalculatorClass.cs
@@ -4,51 +4,44 @@ using System.Linq;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace CalculatorApplication
-
 {
-    
+
     internal class CalculatorClass
     {
-        
 
         public delegate X Formula<X>(X arg1, X arg2);
         public Formula<double> info;
 
-         double numb1, numb2;
-        public  double GetSum(double sum) 
-        {  
-            return numb1 + numb2; 
-        }
-
-        public double GetDifference(double diff) 
-        { 
-            return numb1 - numb2; 
-        }
-
-        public double GetProduct(double product)
+        public double GetSum(double num1, double num2)
         {
-            return numb1 * numb2;
+            return num1 + num2;
         }
-        public double GetQuotient(double ans)
+        public double GetDifference(double num1, double num2)
         {
-            return numb1 / numb2;
+            return num1 - num2;
         }
-
+        public double GetProduct(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+        public double GetQuotient(double num1, double num2)
+        {
+            return num1 / num2;
+        }
         public Formula<double> _info;
-        public event EventHandler CalculateEvent 
-        { 
-            add 
-                { 
-                    Console.WriteLine("Added the Delegate"); 
-                    CalculateEvent += value;
-                }
-            remove { 
-                Console.WriteLine("Removed the Delegate");
+        public event EventHandler CalculateEvent
+        {
+            add
+            {
+                Console.WriteLine("Added the Delegate");
                 CalculateEvent += value;
             }
-        }        
+            remove
+            {
+                Console.WriteLine("Removed the Delegate");
+                CalculateEvent -= value;
+            }
         }
     }
-
+}
